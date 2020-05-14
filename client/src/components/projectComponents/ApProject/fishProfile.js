ed9e719332828797a31f04799f3cc3aab46306f1
DinDim2020 <dmitry.radyukin@gmail.com>
2020-05-14 15:49:25 +0300

    Trevor changes in AP-DinDim branch

--- client/src/components/projectComponents/ApProject/fishProfile.js
+++ client/src/components/projectComponents/ApProject/fishProfile.js
@@ -1,34 +1,38 @@
 import React from 'react';
-import github from "../../../images/UIHere.8acef598.png";
+import github from "./Assets/UIHere.8acef598.png";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-
-const FishProfile = (props) =>{
+function FishProfile() {
+    return <TemperatureAdvice />;
+}
+function TemperatureAdvice() {
     return (<div>
+            <h4 className="reading-box">System Settings: Trout</h4>
+            <Row>
 
-    <Row>
-        <Col>
-            <img alt="Github icon"
-                 className="fish-image"
-                 src={github}/>
-            <p className="reading-box">{props.fishParams.fish_name}</p>
+                <Col lg={6}>
+                    <img alt="Github icon"
+                         className="fish-image"
+                         src={github}/>
 
-        </Col>
-        
-       
-        <Col>
-        <p className="reading-box text-left">Ideal temp: {props.fishParams.temp_target} Degrees<br/>
-            Ideal Ph: {props.fishParams.ph_target} <br/>
-            Ideal Ammonia: {props.fishParams.nh3_target} </p>
-            
-    </Col>
-    
-    </Row>
-            <p className="reading-box ">See our caring for <a href="#">{props.fishParams.fish_name}</a> page.</p>
-    
-    </div>
-    
-    
+
+                </Col>
+
+
+                <Col lg={6}>
+                    <p className="reading-box text-left">Ideal temp: 12 Degrees<br/>
+                        Ideal Ph: 7.3 <br/>
+                        Ideal Ammonia: .0 </p>
+
+                </Col>
+                <Col lg={12}><p className="reading-box ">See our caring for <a href="#">trout</a> page.</p></Col>
+
+            </Row>
+
+
+        </div>
+
+
     );
 }
-export default FishProfile;
+export default TemperatureAdvice;
